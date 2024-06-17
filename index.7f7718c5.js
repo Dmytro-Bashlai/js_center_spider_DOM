b7f58485460ff4d46a6823180cc81cb2ba6bd26b
Dmytro Bashlai <imaxs2016@gmail.com>
2024-06-18 00:18:32 +0300

    make build

---
+++ index.7f7718c5.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=e.clientWidth/2-t.clientWidth/2,i=e.clientHeight/2-t.clientHeight/2;t.style.top=i+"px",t.style.left=l+"px";
+//# sourceMappingURL=index.7f7718c5.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const fromLeft = wall.clientWidth / 2 - spider.clientWidth / 2;
+const fromTop = wall.clientHeight / 2 - spider.clientHeight / 2;
+spider.style.top = fromTop + "px";
+spider.style.left = fromLeft + "px";
+
+//# sourceMappingURL=index.f75de5e1.js.map
